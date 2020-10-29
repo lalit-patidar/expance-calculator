6c8f31780a2a04d88210795e58d3f53a0d4b3667
lalit patidar <patidarlalit460@gmail.com>
2020-10-29 21:43:50 +0530

    setup for heroku app

--- apply/apply.js
+++ apply/apply.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
+const port = process.env.PORT || 1300
 
 const publicDirectoryPath = path.join(__dirname, '../public')
 console.log(publicDirectoryPath)
@@ -12,6 +13,6 @@ app.get('/', (req, res) => {
     
 })
 
-app.listen(1300, () => {
+app.listen(port, () => {
     console.log('server created successfully and runnig at 1300 port')
 })
